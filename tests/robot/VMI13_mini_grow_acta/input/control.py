a1d2a441a6e1c99e0b3de44dfae7597918d02b6f
Tapani Stylman <tapani.stylman@gofore.com>
2025-10-06 14:09:24 +0300

    test: add robot test for sequential grow_acta

---
+++ tests/robot/VMI13_mini_grow_acta/input/control.py
@@ -0,0 +1,52 @@
+from lukefi.metsi.data.vectorize import vectorize
+from lukefi.metsi.domain.natural_processes.grow_acta import grow_acta
+from lukefi.metsi.domain.pre_ops import generate_reference_trees, preproc_filter, scale_area_weight
+from lukefi.metsi.sim.generators import Event
+from lukefi.metsi.sim.simulation_instruction import SimulationInstruction
+
+
+control_structure = {
+    "app_configuration": {
+        "state_format": "vmi13",  # options: fdm, vmi12, vmi13, xml, gpkg
+        # "state_input_container": "csv",  # Only relevant with fdm state_format. Options: pickle, json
+        "state_output_container": "csv",  # options: pickle, json, csv, null
+        # "derived_data_output_container": "pickle",  # options: pickle, json, null
+        "formation_strategy": "partial",
+        "evaluation_strategy": "depth",
+        "run_modes": ["preprocess", "simulate"]
+    },
+    "preprocessing_operations": [
+        scale_area_weight,
+        generate_reference_trees,  # reference trees from strata, replaces existing reference trees
+        preproc_filter,
+        vectorize
+        # "supplement_missing_tree_heights",
+        # "supplement_missing_tree_ages",
+        # "generate_sapling_trees_from_sapling_strata"
+    ],
+    "preprocessing_params": {
+        generate_reference_trees: [
+            {
+                "n_trees": 10,
+                "method": "weibull",
+                "debug": False
+            }
+        ],
+        preproc_filter: [
+            {
+                "remove trees": "sapling or stems_per_ha == 0",
+                "remove stands": "(site_type_category == None) or (site_type_category == 0)",  # not reference_trees
+            }
+        ]
+    },
+    "simulation_instructions": [
+        SimulationInstruction(
+            time_points=[2020, 2025, 2030],
+            events=[
+                Event(grow_acta)
+            ]
+        )
+    ]
+}
+
+__all__ = ['control_structure']
