cad6f20bea44db070ccadf54836eda3875cf7995
Tapani Stylman <tapani.stylman@gofore.com>
2025-10-27 13:21:41 +0200

    fix: fix too long line

--- lukefi/metsi/sim/simulator.py
+++ lukefi/metsi/sim/simulator.py
@@ -7,8 +7,10 @@ from lukefi.metsi.sim.sim_configuration import SimConfiguration
 from lukefi.metsi.sim.simulation_payload import SimulationPayload
 
 
-def simulate_alternatives[T: ComputationalUnit](
-        control: dict[str, Any], units: list[T], db: Optional[sqlite3.Connection] = None) -> dict[str, list[SimulationPayload[T]]]:
+def simulate_alternatives[T: ComputationalUnit](control: dict[str, Any],
+                                                units: list[T],
+                                                db: Optional[sqlite3.Connection] = None) -> dict[str, list[
+                                                    SimulationPayload[T]]]:
     simconfig = SimConfiguration[T](**control)
     nestable_generator = simconfig.full_tree_generators()
     if db is not None:
