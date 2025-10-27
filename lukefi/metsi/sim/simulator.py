2b75bbd8d36f43c19dc1b2257f20df1e228afb1d
Tapani Stylman <tapani.stylman@gofore.com>
2025-10-27 13:07:29 +0200

    ref: remove TypeVar and use new syntax

--- lukefi/metsi/sim/simulator.py
+++ lukefi/metsi/sim/simulator.py
@@ -1,5 +1,5 @@
 import sqlite3
-from typing import Any, Optional, TypeVar
+from typing import Any, Optional
 from lukefi.metsi.app.console_logging import print_logline
 from lukefi.metsi.data.computational_unit import ComputationalUnit
 from lukefi.metsi.domain.collected_data import init_collected_data_tables
@@ -7,11 +7,8 @@ from lukefi.metsi.sim.sim_configuration import SimConfiguration
 from lukefi.metsi.sim.simulation_payload import SimulationPayload
 
 
-T = TypeVar("T", bound=ComputationalUnit)
-
-
-def simulate_alternatives(control: dict[str, Any], units: list[T],
-                          db: Optional[sqlite3.Connection] = None) -> dict[str, list[SimulationPayload[T]]]:
+def simulate_alternatives[T: ComputationalUnit](
+        control: dict[str, Any], units: list[T], db: Optional[sqlite3.Connection] = None) -> dict[str, list[SimulationPayload[T]]]:
     simconfig = SimConfiguration[T](**control)
     nestable_generator = simconfig.full_tree_generators()
     if db is not None:
