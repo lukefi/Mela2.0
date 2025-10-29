e78e29b50cecd8bc67ce5fa1220657dd373fab20
Tapani Stylman <tapani.stylman@gofore.com>
2025-10-29 18:16:32 +0200

    test: refactor VMI13_mini test

--- tests/robot/DatabaseCompareLibrary.py
+++ tests/robot/DatabaseCompareLibrary.py
@@ -29,16 +29,40 @@ def _compare(ref: str, res: str, select: str, numeric_tolerance = 0.0):
 
 
 def node_tables_should_be_equal(ref: str, res: str):
+    """Asserts that the `nodes` tables in the reference and result databases are identical, row by row.
+
+    Args:
+        ref (str): path to the reference database
+        res (str): path to the result database
+    """
     _compare(ref, res, SELECT_NODES)
 
 
 def stand_tables_should_be_equal(ref: str, res: str, numeric_tolerance: float):
+    """Asserts that the `stands` tables in the reference and result databases are identical, row by row.
+
+    Args:
+        ref (str): path to the reference database
+        res (str): path to the result database
+    """
     _compare(ref, res, SELECT_STANDS, numeric_tolerance)
 
 
 def stratum_tables_should_be_equal(ref: str, res: str, numeric_tolerance: float):
+    """Asserts that the `strata` tables in the reference and result databases are identical, row by row.
+
+    Args:
+        ref (str): path to the reference database
+        res (str): path to the result database
+    """
     _compare(ref, res, SELECT_STRATA, numeric_tolerance)
 
 
 def tree_tables_should_be_equal(ref: str, res: str, numeric_tolerance: float):
+    """Asserts that the `trees` tables in the reference and result databases are identical, row by row.
+
+    Args:
+        ref (str): path to the reference database
+        res (str): path to the result database
+    """
     _compare(ref, res, SELECT_TREES, numeric_tolerance)
