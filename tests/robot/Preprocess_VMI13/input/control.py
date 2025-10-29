b1c61c399362ab4815d9f5bcf13b6c4b924a5201
Tapani Stylman <tapani.stylman@gofore.com>
2025-10-29 17:56:33 +0200

    test: add robot test for VMI13 preprocessing

---
+++ tests/robot/Preprocess_VMI13/input/control.py
@@ -0,0 +1,44 @@
+from lukefi.metsi.data.vectorize import vectorize
+from lukefi.metsi.domain.pre_ops import generate_reference_trees, preproc_filter, scale_area_weight
+
+
+control_structure = {
+    "app_configuration": {
+        "state_format": "vmi13",  # options: fdm, vmi12, vmi13, xml, gpkg
+        # "state_input_container": "csv",  # Only relevant with fdm state_format. Options: pickle, json
+        # "state_output_container": "csv",  # options: pickle, json, csv, null
+        # "derived_data_output_container": "pickle",  # options: pickle, json, null
+        "run_modes": ["preprocess", "export_prepro"]
+    },
+    "preprocessing_operations": [
+        scale_area_weight,
+        generate_reference_trees,  # reference trees from strata, replaces existing reference trees
+        preproc_filter,
+        vectorize
+        # "supplement_missing_tree_heights",
+        # "supplement_missing_tree_ages",
+        # "generate_sapling_trees_from_sapling_strata"
+    ],
+    "preprocessing_params": {
+        generate_reference_trees: [
+            {
+                "n_trees": 10,
+                "method": "weibull",
+                "debug": False
+            }
+        ],
+        preproc_filter: [
+            {
+                "remove trees": (lambda tree: tree.sapling or tree.stems_per_ha == 0),
+                # not reference_trees
+                "remove stands": (lambda stand: (stand.site_type_category is None) or (stand.site_type_category == 0))
+            }
+        ]
+    },
+    'export_prepro': {
+        'csv': {},
+        'rst': {}
+    }
+}
+
+__all__ = ['control_structure']
