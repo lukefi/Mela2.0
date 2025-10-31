2b5c2220e9c1e0f7de6c3d36ece03eb83c321812
Tapani Stylman <tapani.stylman@gofore.com>
2025-10-31 12:35:48 +0200

    test: delete unnecessary comments from robot test control files

--- tests/robot/Preprocess_VMI13/input/control.py
+++ tests/robot/Preprocess_VMI13/input/control.py
@@ -4,20 +4,14 @@ from lukefi.metsi.domain.pre_ops import generate_reference_trees, preproc_filter
 
 control_structure = {
     "app_configuration": {
-        "state_format": "vmi13",  # options: fdm, vmi12, vmi13, xml, gpkg
-        # "state_input_container": "csv",  # Only relevant with fdm state_format. Options: pickle, json
-        # "state_output_container": "csv",  # options: pickle, json, csv, null
-        # "derived_data_output_container": "pickle",  # options: pickle, json, null
+        "state_format": "vmi13", 
         "run_modes": ["preprocess", "export_prepro"]
     },
     "preprocessing_operations": [
         scale_area_weight,
-        generate_reference_trees,  # reference trees from strata, replaces existing reference trees
+        generate_reference_trees,
         preproc_filter,
         vectorize
-        # "supplement_missing_tree_heights",
-        # "supplement_missing_tree_ages",
-        # "generate_sapling_trees_from_sapling_strata"
     ],
     "preprocessing_params": {
         generate_reference_trees: [
@@ -30,7 +24,6 @@ control_structure = {
         preproc_filter: [
             {
                 "remove trees": (lambda tree: tree.sapling or tree.stems_per_ha == 0),
-                # not reference_trees
                 "remove stands": (lambda stand: (stand.site_type_category is None) or (stand.site_type_category == 0))
             }
         ]
--- tests/robot/VMI13_mini_db/input/control.py
+++ tests/robot/VMI13_mini_db/input/control.py
@@ -7,22 +7,16 @@ from lukefi.metsi.sim.simulation_instruction import SimulationInstruction
 
 control_structure = {
     "app_configuration": {
-        "state_format": "vmi13",  # options: fdm, vmi12, vmi13, xml, gpkg
-        # "state_input_container": "csv",  # Only relevant with fdm state_format. Options: pickle, json
-        # "state_output_container": "csv",  # options: pickle, json, csv, null
-        # "derived_data_output_container": "pickle",  # options: pickle, json, null
+        "state_format": "vmi13",
         "formation_strategy": "partial",
         "evaluation_strategy": "depth",
         "run_modes": ["preprocess", "simulate"]
     },
     "preprocessing_operations": [
         scale_area_weight,
-        generate_reference_trees,  # reference trees from strata, replaces existing reference trees
+        generate_reference_trees,
         preproc_filter,
         vectorize
-        # "supplement_missing_tree_heights",
-        # "supplement_missing_tree_ages",
-        # "generate_sapling_trees_from_sapling_strata"
     ],
     "preprocessing_params": {
         generate_reference_trees: [
@@ -35,7 +29,6 @@ control_structure = {
         preproc_filter: [
             {
                 "remove trees": (lambda tree: tree.sapling or tree.stems_per_ha == 0),
-                # not reference_trees
                 "remove stands": (lambda stand: (stand.site_type_category is None) or (stand.site_type_category == 0)),
             }
         ]
