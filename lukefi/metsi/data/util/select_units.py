fix: fix step calculation for level mode

lukefi/metsi/data/util/select_units.py
@@ -393,7 +393,7 @@ def _init_search(mode: str,
         a_max = np.max(1 - y)
         a_min = np.min(-1 * y)
         scale = (a_max + a_min) / 2
-        step = abs(scale)
+        step = min(1.0, np.maximum(a_max, -a_min))
         if step < 0.001:
             step = a_max
         y0 = y