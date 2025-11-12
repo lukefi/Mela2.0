4c48ae15bdb3cd16081f3797747047bcb19bf7ea
Tapani Stylman <tapani.stylman@gofore.com>
2025-11-12 13:05:15 +0200

    ref: pass whole SimConfiguration to _simulate_unit

--- lukefi/metsi/sim/simulator.py
+++ lukefi/metsi/sim/simulator.py
@@ -3,9 +3,7 @@ from typing import Any, Optional
 from lukefi.metsi.data.computational_unit import ComputationalUnit
 from lukefi.metsi.sim.event_tree import output_node_to_db
 from lukefi.metsi.sim.collected_data import init_collected_data_tables
-from lukefi.metsi.sim.condition import Condition
-from lukefi.metsi.sim.sim_configuration import SimConfiguration, TransitionFn
-from lukefi.metsi.sim.simulation_instruction import SimulationInstruction
+from lukefi.metsi.sim.sim_configuration import SimConfiguration
 from lukefi.metsi.sim.simulation_payload import SimulationPayload
 
 
@@ -17,35 +15,29 @@ def simulate_alternatives[T: ComputationalUnit](control: dict[str, Any],
     if db is not None:
         init_collected_data_tables(db, simconfig.collected_data)
 
-    instructions = simconfig.instructions
-    end_condition = simconfig.end_condition
-    transition = simconfig.transition
-
     for unit in units:
         payload = SimulationPayload(unit)
         if db is not None:
             output_node_to_db(db, payload, [])
-        _simulate_unit(payload, transition.transition_fn, end_condition, instructions, db)
+        _simulate_unit(payload, simconfig, db)
 
 
 def _simulate_unit[T: ComputationalUnit](payload: SimulationPayload[T],
-                                         transition: TransitionFn[T],
-                                         end_condition: Condition[T],
-                                         instructions: list[SimulationInstruction[T]],
+                                         config: SimConfiguration[T],
                                          db: Optional[sqlite3.Connection] = None) -> list[SimulationPayload[T]]:
     retval = []
-    if not end_condition(payload.computational_unit):
+    if not config.end_condition(payload.computational_unit):
         offset = 0
-        for instruction in instructions:
+        for instruction in config.instructions:
             if all(condition(payload) for condition in instruction.conditions):
                 for new_branch in instruction.unwrap(payload, offset, db):
                     offset += 1
-                    new_branch.computational_unit, _ = transition(new_branch.computational_unit)
-                    retval.extend(_simulate_unit(new_branch, transition, end_condition, instructions, db))
+                    new_branch.computational_unit, _ = config.transition(new_branch.computational_unit)
+                    retval.extend(_simulate_unit(new_branch, config, db))
             else:
                 # Conditions failed. Don't kill the branch but carry on with transition.
-                payload.computational_unit, _ = transition(payload.computational_unit)
-                retval.extend(_simulate_unit(payload, transition, end_condition, instructions, db))
+                payload.computational_unit, _ = config.transition(payload.computational_unit)
+                retval.extend(_simulate_unit(payload, config, db))
     else:
         retval = [payload]
     return retval
