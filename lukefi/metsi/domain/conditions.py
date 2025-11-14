04f2e680d533928a82296222ec6e8770edfb567f
Tapani Stylman <tapani.stylman@gofore.com>
2025-11-14 16:32:58 +0200

    modify conditions.py
    - rename MinimumTimeInterval to TimeSinceTreatment
    - add TimeSinceTag
    - reorder items

--- lukefi/metsi/domain/conditions.py
+++ lukefi/metsi/domain/conditions.py
@@ -1,26 +1,8 @@
 from typing import Optional
 from lukefi.metsi.data.computational_unit import ComputationalUnit
 from lukefi.metsi.sim.condition import Condition
-from lukefi.metsi.sim.generators import TreatmentFn
-from lukefi.metsi.sim.simulation_payload import SimulationPayload
-
-
-class MinimumTimeInterval[T: ComputationalUnit](Condition[SimulationPayload[T]]):
-    def __init__(self, minimum_time: int, treatment: TreatmentFn[T]) -> None:
-        super().__init__(lambda x: _check_eligible_to_run(x, treatment, minimum_time))
-
-
-def _get_operation_last_run[T: ComputationalUnit](operation_history: list[tuple[int, TreatmentFn[T], dict[str, dict]]],
-                                                  operation_tag: TreatmentFn[T]) -> Optional[int]:
-    return next((t for t, o, _ in reversed(operation_history) if o == operation_tag), None)
-
-
-def _check_eligible_to_run[T: ComputationalUnit](
-        payload: SimulationPayload[T],
-        treatment: TreatmentFn[T],
-        minimum_time_interval: int) -> bool:
-    last_run = _get_operation_last_run(payload.operation_history, treatment)
-    return last_run is None or minimum_time_interval <= (payload.computational_unit.time - last_run)
+from lukefi.metsi.sim.simulation_payload import OperationHistory, SimulationPayload
+from lukefi.metsi.sim.treatment import TreatmentFn
 
 
 class TimePoints[T: ComputationalUnit](Condition[SimulationPayload[T]]):
@@ -31,3 +13,39 @@ class TimePoints[T: ComputationalUnit](Condition[SimulationPayload[T]]):
 class RelativeTimePoints[T: ComputationalUnit](Condition[SimulationPayload[T]]):
     def __init__(self, relative_time_points: list[int]) -> None:
         super().__init__(lambda x: x.computational_unit.relative_time in relative_time_points)
+
+
+class TimeSinceTreatment[T: ComputationalUnit](Condition[SimulationPayload[T]]):
+    def __init__(self, minimum_time: int, treatment: TreatmentFn[T]) -> None:
+        super().__init__(lambda x: _check_treatment_last_run(x, treatment, minimum_time))
+
+
+class TimeSinceTag[T: ComputationalUnit](Condition[SimulationPayload[T]]):
+    def __init__(self, minimum_time: int, tag: str) -> None:
+        super().__init__(lambda x: _check_tag_last_run(x, tag, minimum_time))
+
+
+def _check_treatment_last_run[T: ComputationalUnit](
+        payload: SimulationPayload[T],
+        treatment: TreatmentFn[T],
+        minimum_time_interval: int) -> bool:
+    last_run = _get_treatment_last_run(payload.operation_history, treatment)
+    return last_run is None or minimum_time_interval <= (payload.computational_unit.time - last_run)
+
+
+def _get_treatment_last_run[T: ComputationalUnit](operation_history: OperationHistory,
+                                                  treatment: TreatmentFn[T]) -> Optional[int]:
+    return next((t for t, o, _, _ in reversed(operation_history) if o == treatment.__name__), None)
+
+
+def _check_tag_last_run[T: ComputationalUnit](
+    payload: SimulationPayload[T],
+    tag: str,
+    minimum_time_interval: int
+) -> bool:
+    last_run = _get_tag_last_run(payload.operation_history, tag)
+    return last_run is None or minimum_time_interval <= (payload.computational_unit.time - last_run)
+
+
+def _get_tag_last_run(operation_history: OperationHistory, tag: str) -> Optional[int]:
+    return next((time for time, _, _, tags in reversed(operation_history) if tag in tags), None)
