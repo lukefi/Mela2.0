357ceb483178186cf1b339f61b9efcb5581d6c1b
Tapani Stylman <tapani.stylman@gofore.com>
2025-11-14 16:29:21 +0200

    ref: rename operation -> treatment

--- lukefi/metsi/sim/processor.py
+++ lukefi/metsi/sim/processor.py
@@ -1,17 +1,15 @@
 from copy import deepcopy
-from typing import TYPE_CHECKING
 from lukefi.metsi.app.utils import ConditionFailed
 from lukefi.metsi.data.computational_unit import ComputationalUnit
 from lukefi.metsi.sim.collected_data import CollectedData
 from lukefi.metsi.sim.condition import Condition
 from lukefi.metsi.sim.simulation_payload import SimulationPayload
-if TYPE_CHECKING:
-    from lukefi.metsi.sim.generators import TreatmentFn
+from lukefi.metsi.sim.treatment import PreparedTreatment
 
 
 def processor[T: ComputationalUnit](payload: SimulationPayload[T],
-                                    operation: "TreatmentFn[T]",
-                                    operation_tag: "TreatmentFn[T]",
+                                    treatment: PreparedTreatment[T],
+                                    treatment_tag: str,
                                     preconditions: list[Condition[SimulationPayload[T]]],
                                     postconditions: list[Condition[SimulationPayload[T]]],
                                     **operation_parameters: dict[str, dict]) -> tuple[SimulationPayload[T],
@@ -19,12 +17,12 @@ def processor[T: ComputationalUnit](payload: SimulationPayload[T],
     """Managed run conditions and history of a simulator operation. Evaluates the operation."""
     for condition in preconditions:
         if not condition(payload):
-            raise ConditionFailed(f'{operation_tag} aborted - condition "{condition}" failed')
+            raise ConditionFailed(f'{treatment_tag} aborted - condition "{condition}" failed')
 
     try:
-        new_state, new_collected_data = operation(payload.computational_unit)
+        new_state, new_collected_data = treatment(payload.computational_unit)
     except UserWarning as e:
-        raise UserWarning(f"Unable to perform operation {operation_tag}, "
+        raise UserWarning(f"Unable to perform operation {treatment_tag}, "
                           f"at time point {payload.computational_unit.time}; reason: {e}") from e
 
     new_state.update_aggregates()
@@ -37,8 +35,12 @@ def processor[T: ComputationalUnit](payload: SimulationPayload[T],
 
     for condition in postconditions:
         if not condition(newpayload):
-            raise ConditionFailed(f'{operation_tag} aborted - condition "{condition}" failed')
+            raise ConditionFailed(f'{treatment_tag} aborted - condition "{condition}" failed')
 
-    newpayload.operation_history.append((payload.computational_unit.time, operation_tag, operation_parameters))
+    newpayload.operation_history.append(
+        (payload.computational_unit.time,
+         treatment_tag,
+         operation_parameters,
+         treatment.tags))
 
     return newpayload, new_collected_data
