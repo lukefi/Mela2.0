f66e8f0ad7d72470c0af2b105c9e6d36ba782783
Tapani Stylman <tapani.stylman@gofore.com>
2025-11-18 20:52:39 +0200

    test: add unit test for tag combining

--- tests/sim/condition_test.py
+++ tests/sim/condition_test.py
@@ -2,11 +2,11 @@ from typing import Any
 import unittest
 
 from lukefi.metsi.domain.conditions import _get_tag_last_run, _get_treatment_last_run
-from lukefi.metsi.sim.collected_data import OpTuple
+from lukefi.metsi.sim.collected_data import CollectedData, OpTuple
 from lukefi.metsi.sim.condition import Condition
 from lukefi.metsi.sim.generators import Alternatives, Sequence, Event
 from lukefi.metsi.sim.simulation_payload import SimulationPayload
-from lukefi.metsi.sim.treatment import Treatment
+from lukefi.metsi.sim.treatment import PreparedTreatment, Treatment
 from tests.toy_model import ToyModel
 
 
@@ -127,3 +127,19 @@ class ConditionTest(unittest.TestCase):
         self.assertEqual(_get_tag_last_run(operation_history, "2"), 6)
         self.assertEqual(_get_tag_last_run(operation_history, "3"), 8)
         self.assertEqual(_get_tag_last_run(operation_history, "4"), None)
+
+    def test_prepared_treatment_tag_combinations(self):
+        def dummy_fn(x: ToyModel, **kwargs) -> tuple[ToyModel, list[CollectedData]]:
+            _ = kwargs
+            return x, []
+
+        dummy_1 = Treatment(dummy_fn, "dummy", {"default_tag_1"})
+        dummy_2 = Treatment(dummy_fn, "dummy", {"default_tag_2"})
+
+        prepared_dummy_1 = PreparedTreatment(dummy_1, {"additional_tag_1", "additional_tag_2"})
+        prepared_dummy_2 = PreparedTreatment(dummy_1, {"additional_tag_1", "additional_tag_3"})
+        prepared_dummy_3 = PreparedTreatment(dummy_2, {"additional_tag_3", "additional_tag_4"})
+
+        self.assertSetEqual({"default_tag_1", "additional_tag_1", "additional_tag_2"}, prepared_dummy_1.tags)
+        self.assertSetEqual({"default_tag_1", "additional_tag_1", "additional_tag_3"}, prepared_dummy_2.tags)
+        self.assertSetEqual({"default_tag_2", "additional_tag_3", "additional_tag_4"}, prepared_dummy_3.tags)
