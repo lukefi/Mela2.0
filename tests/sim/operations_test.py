2fd489f64117d0cf0f3f7581a76191da5b7e4c1e
Tapani Stylman <tapani.stylman@gofore.com>
2025-11-28 16:10:46 +0200

    test: remove obsolete unit test

--- tests/sim/operations_test.py
+++ tests/sim/operations_test.py
@@ -1,6 +1,6 @@
 import unittest
-from lukefi.metsi.sim.operations import prepared_treatment, simple_processable_chain
-from tests.toy_model import ToyModel, parametrized_treatment
+from lukefi.metsi.sim.operations import simple_processable_chain
+from tests.toy_model import parametrized_treatment
 
 
 class TestOperations(unittest.TestCase):
@@ -12,15 +12,3 @@ class TestOperations(unittest.TestCase):
                           simple_processable_chain,
                           operation_tags,
                           operation_params)
-
-    def test_prepared_treatment(self):
-        assertions = [
-            ([ToyModel("", 10), {}], 10),
-            ([ToyModel("", 10), {'amplify': True}], 10000),
-            ([ToyModel("", 10), {'amplify': False}], 10)
-        ]
-
-        for case in assertions:
-            function = prepared_treatment(parametrized_treatment, **case[0][1])
-            result = function(case[0][0])
-            self.assertEqual(case[1], result[0].value)
