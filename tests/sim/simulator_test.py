f01f82cfc82010694c56d31ef5a4387046ea7743
Ari-Antti Tervonen <ari-antti.tervonen@kajapro.fi>
2025-12-01 12:37:48 +0200

    Renamed test parameter

--- tests/sim/simulator_test.py
+++ tests/sim/simulator_test.py
@@ -383,7 +383,7 @@ class SimulatorTest(unittest.TestCase):
             "simulation_instructions": [
                 SimulationInstruction(
                     events=[
-                        Event(toy_inc, parameters={
+                        Event(toy_inc, static_parameters={
                             "incrementation": 1
                         })
                     ]
@@ -391,10 +391,10 @@ class SimulatorTest(unittest.TestCase):
                 SimulationInstruction(
                     events=[
                         Alternatives([
-                            Event(toy_inc, parameters={
+                            Event(toy_inc, static_parameters={
                                 "incrementation": 2
                             }),
-                            Event(toy_inc, parameters={
+                            Event(toy_inc, static_parameters={
                                 "incrementation": 3
                             }),
                         ])
