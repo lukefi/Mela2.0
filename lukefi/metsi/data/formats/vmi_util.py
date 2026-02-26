61037f353ed536f042aeb757081f88cd5b23823e
Ari-Antti Tervonen <ari-antti.tervonen@kajapro.fi>
2026-02-26 12:45:21 +0200

    VMI9 origin update

--- lukefi/metsi/data/formats/vmi_util.py
+++ lukefi/metsi/data/formats/vmi_util.py
@@ -479,6 +479,17 @@ def determine_stratum_origin(source_origin: str) -> int:
     return 0
 
 
+def determine_stratum_origin_vmi9(source_origin: str) -> str:
+
+    if source_origin in ("1", "2"):
+        return "0"  # Natural
+    if source_origin in ("3", "5", "7", "8"):
+        return "2"  # Planted
+    if source_origin in ("4", "6"):
+        return "1"  # Seeded
+    return ""
+
+
 def determine_stratum_age_values(biological_age_source: str,
                                  breast_height_age_source: str,
                                  height: Optional[float]) -> tuple[float, float]:
@@ -1125,7 +1136,8 @@ def append_vmi9_strata_from_stand_row(
         ppa_total = jakso1_ppa if seg_no == 1 else jakso2_ppa
 
         asema = stand_row[indices[f"jakso{seg_no}_asema"]]
-        synty = stand_row[indices[f"jakso{seg_no}_syntytapa"]]
+        synty = determine_stratum_origin_vmi9(stand_row[indices[f"jakso{seg_no}_syntytapa"]])
+
         stems1000 = stand_row[indices[f"jakso{seg_no}_kokonaisrunkoluku1000"]]
         d_cm = stand_row[indices[f"jakso{seg_no}_keskilapimitta_cm"]]
         h_dm = stand_row[indices[f"jakso{seg_no}_keskipituus_dm"]]
