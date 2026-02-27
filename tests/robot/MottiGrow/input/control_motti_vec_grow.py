c5e8213a11d7cb1e9b1007b4ef1627f9be2103c4
Ari-Antti Tervonen <ari-antti.tervonen@kajapro.fi>
2026-02-27 11:59:57 +0200

    updated motti robot test

--- tests/robot/MottiGrow/input/control_motti_vec_grow.py
+++ tests/robot/MottiGrow/input/control_motti_vec_grow.py
@@ -1,16 +1,15 @@
 from lukefi.metsi.data.model import ForestStand
-from lukefi.metsi.domain.conditions import TimePoints
 from lukefi.metsi.domain.pre_ops import (
     compute_location_metadata,
     generate_reference_trees,
     scale_area_weight,
     preproc_filter)
-from lukefi.metsi.domain.events import GrowMotti
+from lukefi.metsi.domain.natural_processes.grow_motti_dll import grow_motti_dll_fn
 from lukefi.metsi.sim.condition import Condition
-from lukefi.metsi.sim.operations import do_nothing
+from lukefi.metsi.sim.generators import Alternatives, Event, Sequence
 from lukefi.metsi.sim.sim_configuration import Transition
 from lukefi.metsi.sim.simulation_instruction import SimulationInstruction
-
+from lukefi.metsi.sim.treatment import do_nothing
 
 control_structure = {
     "app_configuration": {
@@ -39,20 +38,19 @@ control_structure = {
         ]
     },
     "simulation_instructions": [
-
         SimulationInstruction(
-            conditions=[TimePoints([2020, 2025, 2030])],
             events=[
-                GrowMotti(
-                    parameters={
-                        "step": 5,
-                    }
-
-                )
+                Alternatives([
+                    Event(treatment=do_nothing, static_parameters={"n": 1}, tags={"first_type"}),
+                    Sequence([
+                        Event(treatment=do_nothing, static_parameters={"n": 2}, tags={"second_type"}, db_output=True),
+                        Event(treatment=do_nothing, static_parameters={"n": 3}, tags={"third_type"}, db_output=True)
+                    ])
+                ])
             ]
         )
     ],
-    "transition": Transition[ForestStand](do_nothing),
+    "transition": Transition(grow_motti_dll_fn),
     "end_condition": Condition[ForestStand](lambda x: x.computational_unit.year > 2030)
 }
 
