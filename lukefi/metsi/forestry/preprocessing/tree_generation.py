ea9b95bddfa73fbf0ef74608cad4b41823f44fe0
Tapani Stylman <131968101+TStylman@users.noreply.github.com>
2026-03-19 17:07:13 +0200

    feat: height distribution stems scaling (#66)
    Generated reference tree `stems_per_ha` is now scaled by ratio of
    stratum basal area to sum of reference tree basal areas.

--- lukefi/metsi/forestry/preprocessing/tree_generation.py
+++ lukefi/metsi/forestry/preprocessing/tree_generation.py
@@ -139,6 +139,11 @@ def reference_trees_from_tree_stratum(stand: ForestStand, stratum: TreeStratum, 
 
     if strategy == TreeStrategy.HEIGHT_DISTRIBUTION:
         result = _trees_from_sapling_height_distribution(stratum, params["n_trees"])
+        if params.get("scale_height_distribution_stems_by_ba", True):
+            assert stratum.basal_area is not None
+            if result.breast_height_diameter.any():
+                result.stems_per_ha = result.stems_per_ha * stratum.basal_area / \
+                    _calculate_basal_area_from_trees(result.stems_per_ha, result.breast_height_diameter)
 
     elif strategy == TreeStrategy.WEIBULL_DISTRIBUTION:
         result = _trees_from_weibull(stratum, params["n_trees"])
