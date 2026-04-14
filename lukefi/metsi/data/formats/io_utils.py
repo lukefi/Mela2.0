0df7cbe624378899ebc0fe7cb23c72664c86a844
Tapani Stylman <tapani.stylman@gofore.com>
2026-04-14 17:47:04 +0300

    inline `rst_float(2)`

--- lukefi/metsi/data/formats/io_utils.py
+++ lukefi/metsi/data/formats/io_utils.py
@@ -74,7 +74,7 @@ def msb_metadata(stand: ForestStand) -> tuple[list[str], list[str], list[str]]:
 
 def c_var_metadata(uid: float | None, cvars_len: int) -> list[str]:
     total_length = 2 + cvars_len
-    cvars_meta = [rst_float(uid or 0), str(total_length), rst_float(2), rst_float(cvars_len)]
+    cvars_meta = [rst_float(uid or 0), str(total_length), "2.000000", rst_float(cvars_len)]
     return list(cvars_meta)
 
 
