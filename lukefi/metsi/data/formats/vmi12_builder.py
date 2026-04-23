a5f1c539ce9007970bb97541f89f606eb9f93073
Tapani Stylman <tapani.stylman@gofore.com>
2026-04-23 19:44:17 +0300

    implement trees and strata for vmi12

--- lukefi/metsi/data/formats/vmi12_builder.py
+++ lukefi/metsi/data/formats/vmi12_builder.py
@@ -1,12 +1,13 @@
 from typing import override
 from lukefi.metsi.app.utils import MetsiException
 from lukefi.metsi.data.conversion import vmi2internal
+from lukefi.metsi.data.enums.vmi import VmiIteration
 from lukefi.metsi.data.formats import util, vmi_util
 from lukefi.metsi.data.formats.declarative_conversion import Conversion
 from lukefi.metsi.data.formats.forest_builder_base import RowKind, VMIBuilder
 from lukefi.metsi.data.formats.vmi_const import VMI12_STAND_INDICES, VMI12_STRATUM_INDICES, VMI12_TREE_INDICES
 from lukefi.metsi.data.model import ForestStand
-from lukefi.metsi.data.vector_model import TreeStrata
+from lukefi.metsi.data.vector_model import ReferenceTrees, TreeStrata
 from lukefi.metsi.domain.forestry_types import StandList
 
 
@@ -45,6 +46,32 @@ class VMI12Builder(VMIBuilder):
                 result[stand.identifier] = stand
             except Exception as e:
                 raise MetsiException(f"Parsing stand row {row} failed: {e}") from e
+
+        if self.builder_flags.get("strata", False):
+            for stand_identifier, stand_stratum_rows in self.stratum_rows.items():
+                strata = TreeStrata(len(stand_stratum_rows))
+                for j, stand_stratum_row in enumerate(stand_stratum_rows):
+                    try:
+                        self._convert_stratum_entry(strata, stand_stratum_row, j)
+                    except Exception as e:
+                        raise MetsiException(f"Parsing stratum row {stand_stratum_row} failed: {e}") from e
+
+                result[stand_identifier].tree_strata = strata
+
+        if self.builder_flags.get("measured_trees", False):
+            for stand_identifier, stand_tree_rows in self.tree_rows.items():
+                stand_ = result.get(stand_identifier)
+                if stand_ is None:
+                    continue
+
+                trees = ReferenceTrees(len(stand_tree_rows))
+                for j, stand_tree_row in enumerate(stand_tree_rows):
+                    try:
+                        self._convert_tree_entry(trees, stand_tree_row, j, stand_.forestry_centre_id)
+                    except Exception as e:
+                        raise MetsiException(f"Parsing tree row {stand_tree_row} failed: {e}") from e
+
+                result[stand_identifier].reference_trees = trees
 
         return list(result.values())
 
@@ -153,4 +180,117 @@ class VMI12Builder(VMIBuilder):
 
     @staticmethod
     def _convert_stratum_entry(strata: TreeStrata, row: dict[str, str], i: int):
-        pass
+        identifier = vmi_util.generate_stratum_identifier(row)
+
+        species = vmi2internal.convert_species(row["species"])
+        origin = vmi2internal.convert_origin(row["origin"])
+
+        stems_per_ha = util.get_or_default(util.parse_type(row["stems_per_ha"], float), 0.0)
+        sapling_stems_per_ha = util.get_or_default(util.parse_type(row["sapling_stems_per_ha"], float), 0.0)
+
+        mean_diameter = util.parse_type(row["avg_diameter"], float)
+        mean_height = vmi_util.determine_stratum_tree_height(row["avg_height"])
+
+        biological_age, breast_height_age = vmi_util.determine_stratum_age_values(
+            row["biological_age"],
+            row["d13_age"],
+            mean_height
+        )
+
+        basal_area = util.parse_type(row["basal_area"], float)
+        stratum_number = util.parse_int(row["stratum_number"])
+        storey = vmi_util.determine_storey_for_stratum(row["stratum_rank"])
+        stratum_rank = vmi2internal.convert_stratum_rank(row["stratum_rank"])
+
+        strata.identifier[i] = identifier
+        strata.species[i] = species
+        strata.mean_diameter[i] = mean_diameter
+        strata.mean_height[i] = mean_height
+        strata.breast_height_age[i] = breast_height_age
+        strata.biological_age[i] = biological_age
+        strata.stems_per_ha[i] = stems_per_ha
+        strata.basal_area[i] = basal_area
+        if origin is not None:
+            strata.origin[i] = origin
+        if stratum_number is not None:
+            strata.stratum_number[i] = stratum_number
+        if storey is not None:
+            strata.storey[i] = storey
+        strata.sapling_stems_per_ha[i] = sapling_stems_per_ha
+        if stratum_rank is not None:
+            strata.stratum_rank[i] = stratum_rank
+
+
+    @staticmethod
+    def _convert_tree_entry(trees: ReferenceTrees,
+                            row: dict[str, str],
+                            i: int,
+                            forestry_centre_id: int | None,
+                            ahvkeilaus: str | None = None,
+                            height_conversion_factor: float = 100.0,
+                            measured_height_conversion_factor: float = 10.0):
+        identifier = vmi_util.generate_tree_identifier(row)
+        tree_number = util.parse_type(row["tree_number"], int)
+        species = vmi2internal.convert_species(row["species"])
+        tree_category = vmi2internal.convert_tree_category(row["tree_category"])
+
+        breast_height_diameter = vmi_util.transform_tree_diameter(row["diameter"])
+        breast_height_age, biological_age = vmi_util.determine_tree_age_values(
+            row["d13_age"],
+            row["age_increase"],
+            row["total_age"],
+        )
+
+        height = vmi_util.determine_tree_height(
+            row["height"],
+            conversion_factor=height_conversion_factor,
+        )
+        measured_height = vmi_util.determine_tree_height(
+            row["measured_height"],
+            conversion_factor=measured_height_conversion_factor,
+        )
+
+        stems_per_ha = vmi_util.determine_stems_per_ha(breast_height_diameter,
+                                                       vmi_version=VmiIteration.VMI12,
+                                                       forestry_centre_id=forestry_centre_id,
+                                                       ahvkeilaus=ahvkeilaus)
+        origin = vmi2internal.convert_origin(row["origin"])
+
+        management_category = vmi_util.determine_tree_management_category(row["latvuskerros"])
+        storey = vmi_util.determine_storey_for_tree(row["latvuskerros"])
+
+        sapling = False
+        tree_type = vmi2internal.convert_tree_type(row["tree_type"])
+
+        damage_raw = row["tuhon_ilmiasu"]
+        damage_type = None if damage_raw in ("  ", " ", ".", "") else damage_raw.strip()
+        crown_class = vmi2internal.convert_crown_class(row["latvuskerros"])
+
+        trees.identifier[i] = identifier
+        if tree_number is not None:
+            trees.tree_number[i] = tree_number
+        trees.species[i] = species
+        trees.breast_height_diameter[i] = breast_height_diameter
+        if height is not None:
+            trees.height[i] = height
+        if measured_height is not None:
+            trees.measured_height[i] = measured_height
+        if breast_height_age is not None:
+            trees.breast_height_age[i] = breast_height_age
+        if biological_age is not None:
+            trees.biological_age[i] = biological_age
+        trees.stems_per_ha[i] = stems_per_ha
+        if origin is not None:
+            trees.origin[i] = origin
+        trees.management_category[i] = management_category
+        if tree_category is not None:
+            trees.tree_category[i] = tree_category
+        if storey is not None:
+            trees.storey[i] = storey
+        trees.sapling[i] = sapling
+        if tree_type is not None:
+            trees.tree_type[i] = tree_type
+        if damage_type is not None:
+            trees.damage_type[i] = damage_type
+        if crown_class is not None:
+            trees.crown_class[i] = crown_class
