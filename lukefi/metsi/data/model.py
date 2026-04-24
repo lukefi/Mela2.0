cb9238d00f8a01fef35b58577c8058e7aeed6367
Tapani Stylman <131968101+TStylman@users.noreply.github.com>
2026-04-24 13:38:34 +0300

    ref: move `motti_state` copy logic to `finalize` (#91)
    Relocated custom copy logic for Motti state to `finalize` to avoid
    potential side effects (in declarative conversions, for example).

--- lukefi/metsi/data/model.py
+++ lukefi/metsi/data/model.py
@@ -410,6 +410,39 @@ class ForestStand(Finalizable, ComputationalUnit):
         retval = copy(self)
         retval.reference_trees = self.reference_trees.finalize()
         retval.tree_strata = self.tree_strata.finalize()
+
+        if self.motti_state is not None:
+            dll = self.motti_state.dll
+            yy = self.motti_state.yy
+            yp = self.motti_state.yp
+            buffers = self.motti_state.buffers
+            ntrees = self.motti_state.ntrees
+            signature = self.motti_state.signature
+
+            if dll is None or yy is None or yp is None or buffers is None or ntrees is None:
+                retval.motti_state = None
+                return retval
+            if signature is None:
+                retval.motti_state = None
+                return retval
+            try:
+                yy2 = dll.clone_site(yy)
+                yp2 = dll.clone_trees(yp)
+                buffers2 = dll.clone_state_buffers(buffers)
+            except (AttributeError, TypeError, ValueError, RuntimeError):
+                # cloning failed -> drop state rather than share pointers
+                retval.motti_state = None
+                return retval
+
+            retval.motti_state = MottiState(
+                dll=dll,
+                yy=yy2,
+                yp=yp2,
+                ntrees=int(ntrees),
+                buffers=buffers2,
+                signature=cast(tuple[int, ...], signature),
+            )
+
         return retval
 
     @override
@@ -499,50 +532,6 @@ class ForestStand(Finalizable, ComputationalUnit):
         denominator = min(100, sorted_cum_stems[i_100_largest])
 
         return (numerator_1 + numerator_2) / denominator
-
-    def __copy__(self):
-        """Make branch-safe copies: clone Motti CFFI buffers so branches don't share state."""
-        new_obj = self.__class__(**{k: copy(v) for k, v in self.__dict__.items() if k != "motti_state"})
-
-        ms = self.motti_state
-        if ms is None:
-            new_obj.motti_state = None
-            return new_obj
-
-        dll = getattr(ms, "dll", None)
-        yy = getattr(ms, "yy", None)
-        yp = getattr(ms, "yp", None)
-        buffers = getattr(ms, "buffers", None)
-        ntrees = getattr(ms, "ntrees", None)
-        signature = getattr(ms, "signature", None)
-
-        if dll is None or yy is None or yp is None or buffers is None or ntrees is None:
-            new_obj.motti_state = None
-            return new_obj
-        if signature is None:
-            new_obj.motti_state = None
-            return new_obj
-        try:
-            yy2 = dll.clone_site(yy)
-            yp2 = dll.clone_trees(yp)
-            buffers2 = dll.clone_state_buffers(buffers)
-        except (AttributeError, TypeError, ValueError, RuntimeError):
-            # cloning failed -> drop state rather than share pointers
-            new_obj.motti_state = None
-            return new_obj
-
-        new_obj.motti_state = MottiState(
-            dll=dll,
-            yy=yy2,
-            yp=yp2,
-            ntrees=int(ntrees),
-            buffers=buffers2,
-            signature=cast(tuple[int, ...], signature),
-        )
-        return new_obj
-
-    def __deepcopy__(self, memo):
-        return self.__copy__()
 
 
 def stand_as_internal_csv_row(stand: ForestStand, decl_keys: Optional[list[str]] = None) -> list[str]:
