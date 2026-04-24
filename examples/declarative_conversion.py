4806d10e97221891bf399e53f09d6bb46e8caaf2
Tapani Stylman <tapani.stylman@gofore.com>
2026-04-24 13:24:32 +0300

    fix indices in conversion example

--- examples/declarative_conversion.py
+++ examples/declarative_conversion.py
@@ -20,17 +20,17 @@ control_structure = {
         'vmi13': {
             # common conversions
             'VAR0': Conversion(lambda: 123456789),
-            'VAR1': Conversion(lambda x: int(x) * 2, indices=(0,)),
-            'VAR2': Conversion(lambda x: x, indices=(1,)),
-            'VAR3': Conversion(sum3, indices=(2, 3, 4)),
-            'VAR4': Conversion(lambda x, y: pow(int(x), int(y)), indices=(2, 5)),
-            'VAR5': Conversion(lambda x, y: pow(float(x), float(y)), indices=(3, 5)),
+            'VAR1': Conversion(lambda x: int(x) * 2, indices=("row_type",)),
+            'VAR2': Conversion(lambda x: x, indices=("lohkomuoto",)),
+            'VAR3': Conversion(sum3, indices=("lohkomuoto", "section_y", "section_x")),
+            'VAR4': Conversion(lambda x, y: pow(int(x), int(y)), indices=("lohkomuoto", "test_area_number")),
+            'VAR5': Conversion(lambda x, y: pow(float(x), float(y)), indices=("section_y", "test_area_number")),
             'VAR_RANDOM': Conversion(random),
             'VAR_KISSA': Conversion(lambda: "Kissa123"),
-            'VAR8': Conversion(lambda x: str(x) if not isinstance(x, str) else x, indices=(3,)),
+            'VAR8': Conversion(lambda x: str(x) if not isinstance(x, str) else x, indices=("section_y",)),
             # conversions based on object type spesifications
-            'VAR9': Conversion(lambda x, obj: int(x) * obj.area, indices=(0,), object_type=ForestStand),
-            'VAR10': Conversion(lambda x, obj: int(x) * obj.VAR1, indices=(0,), object_type=ForestStand),
+            'VAR9': Conversion(lambda x, obj: int(x) * obj.area, indices=("row_type",), object_type=ForestStand),
+            'VAR10': Conversion(lambda x, obj: int(x) * obj.VAR1, indices=("row_type",), object_type=ForestStand),
         }
     }
 }
