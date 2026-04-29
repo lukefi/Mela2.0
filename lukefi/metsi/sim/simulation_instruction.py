39ecb1abbf44f43aaa5d6146500e349d2e52a9f3
Tapani Stylman <tapani.stylman@gofore.com>
2026-04-29 23:26:02 +0300

    remove unused TypeVar

--- lukefi/metsi/sim/simulation_instruction.py
+++ lukefi/metsi/sim/simulation_instruction.py
@@ -1,13 +1,11 @@
 import sqlite3
-from typing import Generator, Optional, TypeVar
+from typing import Generator, Optional
 from typing import Sequence as Sequence_
 
 from lukefi.metsi.data.computational_unit import ComputationalUnit
 from lukefi.metsi.sim.condition import Condition
 from lukefi.metsi.sim.generators import Alternatives, EventGeneratorBase, EventGenerator, Sequence
 from lukefi.metsi.sim.simulation_payload import SimulationPayload
-
-T = TypeVar('T', bound=ComputationalUnit)  # T = ForestStand
 
 
 class SimulationInstruction[T: ComputationalUnit]:
