a7f6b4783fb48a4cafe97e79e11564f73295b564
Tapani Stylman <tapani.stylman@gofore.com>
2026-04-30 16:42:52 +0300

    remove unused import

--- examples/optional_generator.py
+++ examples/optional_generator.py
@@ -7,7 +7,6 @@ from lukefi.metsi.sim.generators import Alternatives, Event, Optional, Sequence
 from lukefi.metsi.sim.sim_configuration import Transition
 from lukefi.metsi.sim.simulation_instruction import SimulationInstruction
 from lukefi.metsi.sim.treatment import do_nothing
-from examples.declarations.export_prepro import mela_decl
 from examples.declarations.sqlite import sqlite_decl
 
 
