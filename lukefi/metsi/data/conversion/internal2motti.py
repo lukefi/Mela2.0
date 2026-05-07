40a9da57333d4d71190d068870bae357ff7f0e6a
Tapani Stylman <tapani.stylman@gofore.com>
2026-05-07 12:03:10 +0300

    add motti enums and wip conversions

---
+++ lukefi/metsi/data/conversion/internal2motti.py
@@ -0,0 +1,39 @@
+from lukefi.metsi.app.utils import MetsiException
+from lukefi.metsi.data.enums.internal import CONIFEROUS_SPECIES, DECIDUOUS_SPECIES, Storey, TreeSpecies
+from lukefi.metsi.data.enums.motti import MottiSpecies, MottiStorey
+
+
+_SPECIES_MAP = {
+    TreeSpecies.PINE: MottiSpecies.PINE,
+    TreeSpecies.SPRUCE: MottiSpecies.SPRUCE,
+    TreeSpecies.SILVER_BIRCH: MottiSpecies.SILVER_BIRCH,
+    TreeSpecies.DOWNY_BIRCH: MottiSpecies.DOWNY_BIRCH,
+    TreeSpecies.ASPEN: MottiSpecies.ASPEN,
+    TreeSpecies.GREY_ALDER: MottiSpecies.ALDER,
+    TreeSpecies.COMMON_ALDER: MottiSpecies.ALDER
+}
+
+_STOREY_MAP = {
+    Storey.DOMINANT: MottiStorey.DOMINANT,
+    Storey.UNDER: MottiStorey.UNDER,
+    Storey.OVER: MottiStorey.OVER,
+    Storey.SPARE: MottiStorey.SPARE,
+}
+
+
+def convert_species(source: TreeSpecies) -> MottiSpecies:
+    """
+    Map internal TreeSpecies -> Motti species codes directly.
+    - Keep main species 1..5 as-is
+    - Collapse both alders (GREY_ALDER, COMMON_ALDER) to 6
+    - If in CONIFEROUS_SPECIES -> 8
+    - If in DECIDUOUS_SPECIES -> 9
+    """
+    if source in _SPECIES_MAP:
+        return _SPECIES_MAP[source]
+    if source in CONIFEROUS_SPECIES:
+        return MottiSpecies.OTHER_CONIFEROUS
+    if source in DECIDUOUS_SPECIES:
+        return MottiSpecies.OTHER_DECIDUOUS
+
+    raise MetsiException(f"Unable to map internal species {source} to Motti species")
---
+++ lukefi/metsi/data/enums/motti.py
@@ -0,0 +1,19 @@
+from enum import IntEnum
+
+
+class MottiSpecies(IntEnum):
+    PINE = 1
+    SPRUCE = 2
+    SILVER_BIRCH = 3
+    DOWNY_BIRCH = 4
+    ASPEN = 5
+    ALDER = 6
+    OTHER_CONIFEROUS = 8
+    OTHER_DECIDUOUS = 9
+
+
+class MottiStorey(IntEnum):
+    UNDER = 1
+    DOMINANT = 2
+    OVER = 3
+    SPARE = 4
