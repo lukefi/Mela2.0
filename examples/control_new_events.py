1cb6826b9303ccb0fb3168ec02b8add9321f9332
Tapani Stylman <tapani.stylman@gofore.com>
2026-05-12 00:11:57 +0300

    fix example

--- examples/control_new_events.py
+++ examples/control_new_events.py
@@ -1,12 +1,15 @@
 from lukefi.metsi.domain.conditions import TimePoints
 from lukefi.metsi.domain.forestry_types import ForestCondition
 from lukefi.metsi.domain.natural_processes.grow_motti_dll import grow_motti_dll_fn
+from lukefi.metsi.domain.natural_processes.motti_dll_wrapper import Motti4DLL
 from lukefi.metsi.domain.pre_ops import generate_reference_trees
 from lukefi.metsi.sim.sim_configuration import Transition
 from lukefi.metsi.sim.simulation_instruction import SimulationInstruction
 from examples.declarations.sqlite import sqlite_decl
 
 from user_events import Mounding
+
+Motti4DLL.load()
 
 control_structure = {
     "app_configuration": {
