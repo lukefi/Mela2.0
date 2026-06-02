a297ebdcce6c0015c97e7b8f3b288270446389f0
Tapani Stylman <tapani.stylman@gofore.com>
2026-06-02 16:48:53 +0300

    add comments

--- lukefi/metsi/domain/natural_processes/motti_bootstrap.py
+++ lukefi/metsi/domain/natural_processes/motti_bootstrap.py
@@ -2,10 +2,11 @@ from typing import Any
 from lukefi.metsi.data.model import ForestStand
 from lukefi.metsi.domain.natural_processes.motti_util import ensure_state
 
-
+# TODO: why do we need this separate initialization when ensure_state is called in grow_motti_fn every time anyway?
 def initialize_motti(stand: ForestStand, parameters: dict[str, Any]) -> None:
     _ = parameters
     if stand.motti_state is not None:
         return
 
+    # TODO: Why is step=5 when just initializing?
     ensure_state(stand, step=5, sim_year=int(stand.year))
