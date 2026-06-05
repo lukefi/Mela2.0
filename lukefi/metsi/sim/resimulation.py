00427ddd4fd95ff8cde8f7d33d49a4e22e84f155
Tapani Stylman <tapani.stylman@gofore.com>
2026-06-05 16:45:39 +0300

    wip implementation of main resimulation loop

--- lukefi/metsi/sim/resimulation.py
+++ lukefi/metsi/sim/resimulation.py
@@ -1,11 +1,15 @@
+from copy import deepcopy
 import csv
 import sqlite3
 from typing import Any, Set
 
 from lukefi.metsi.data.model import ForestStand
+from lukefi.metsi.sim.simulation_payload import SimulationPayload
+from lukefi.metsi.sim.transition import TransitionFn
 from lukefi.metsi.sim.treatment import PredeterminedTreatment, TreatmentFn
+from lukefi.metsi.sim.updating import get_step_and_treatments
 
-type Schedule = list[list[tuple[int, PredeterminedTreatment[ForestStand]]]]
+type Schedule = list[tuple[int, PredeterminedTreatment[ForestStand]]]
 type Schedules = list[Schedule]
 
 
@@ -25,27 +29,42 @@ def resimulate_schedules(control: dict[str, Any],
                          out_db: sqlite3.Connection):
     _ = out_db
 
-    # TODO: how to reconstruct initial state if and when original simulation db has incomplete attributes?
-    #           - always complete output for initial state?
-    #               - would lead to lots of mostly empty columns...
-    #               - unless we add new tables specifically for the initial state?
-    #           - require original source data or preprocessed csv?
-    #               - how to deal with potential updating?
-    # TODO: how to declare schedules/leaf nodes in control?
-    # TODO: building simulation instructions (or equivalent) from declared leaf nodes/schedules
+    # TODO: exact format of schedules csv not fully specified
+    schedules_file_path = control["selected_schedules_file"]
+
     # TODO: recreating dynamic parameters and other complex structures from original control file
     #           - possible quick hack - declare LUT in resim control?
     #           - no need initially - dynamic parameters needed only in Monte-Carlo
-
-    schedules_file_path = control["selected_schedules_file"]
     treatment_map = control["treatment_map"]
 
+    transition = _determine_transition(control, in_db)
     instructions_per_stand = _recreate_instructions(schedules_file_path, treatment_map, in_db)
+
+    for instructions in instructions_per_stand.values():
+        for schedule in instructions.schedules:
+            current = SimulationPayload(deepcopy(instructions.initial_state))
+            current.computational_unit.predetermined_treatments = schedule
+            time_points = set(time_point for time_point, _ in schedule)
+            for time_point in time_points:
+                step, treatments = get_step_and_treatments(current.computational_unit, time_point)
+
+                if step > 0:
+                    current.computational_unit, cd = transition(current.computational_unit, step)
+
+                for treatment in treatments:
+                    current.computational_unit, cd = treatment(current.computational_unit)
+
+
+def _determine_transition(control: dict[str, Any], in_db: sqlite3.Connection) -> TransitionFn[ForestStand]:
+    _ = control
+    _ = in_db
+    assert False, "_determine_transition not implemented"
 
 
 def _recreate_instructions(sched_file_path: str,
                            treatment_map: dict[str, TreatmentFn],
                            in_db: sqlite3.Connection) -> dict[str, ResimulationInstructions]:
+    # TODO: building simulation instructions (or equivalent) from declared leaf nodes/schedules
     instructions_per_stand: dict[str, ResimulationInstructions] = {}
     with open(sched_file_path, "r", encoding="utf-8") as sched_file:
         sched_reader = csv.reader(sched_file)
@@ -102,7 +121,7 @@ def _recreate_schedule(schedule_row: list[str],
             leaf_node_id
         )
     )
-    retval = []
+    retval: Schedule = []
     for step in cur:
         retval.append(
             (
@@ -135,6 +154,12 @@ def _parse_params(params_str: str) -> dict[str, Any]:
 
 
 def _reconstruct_initial_state(stand_id: str, in_db: sqlite3.Connection) -> ForestStand:
+    # TODO: how to reconstruct initial state if and when original simulation db has incomplete attributes?
+    #           - always complete output for initial state?
+    #               - would lead to lots of mostly empty columns...
+    #               - unless we add new tables specifically for the initial state?
+    #           - require original source data or preprocessed csv?
+    #               - how to deal with potential updating?
     _ = stand_id
     _ = in_db
     assert False, "_reconstruct_initial_state not implemented"
