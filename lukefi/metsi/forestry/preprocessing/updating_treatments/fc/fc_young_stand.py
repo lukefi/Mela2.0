1cd14eddaca81c51cf687a95363328c3bfc95230
Tapani Stylman <tapani.stylman@gofore.com>
2026-06-12 12:54:04 +0300

    replace missing Motti treatments with do_nothing stubs

--- lukefi/metsi/forestry/preprocessing/updating_treatments/fc/fc_young_stand.py
+++ lukefi/metsi/forestry/preprocessing/updating_treatments/fc/fc_young_stand.py
@@ -1,17 +1,21 @@
+from lukefi.metsi.sim.operations import do_nothing
 from lukefi.metsi.sim.treatment import PredeterminedTreatment
-from lukefi.metsi.domain.forestry_treatments.earlycare import earlycare_fn
-from lukefi.metsi.domain.forestry_treatments.pct import pct_fn
+# from lukefi.metsi.domain.forestry_treatments.earlycare import earlycare_fn
+# from lukefi.metsi.domain.forestry_treatments.pct import pct_fn
 
-
+# TODO: earlycare only implemented in Motti
 mechanicalClearing = PredeterminedTreatment(
     name="fc_mechanical_clearing",
-    treatment_fn=earlycare_fn,
+    # treatment_fn=earlycare_fn,
+    treatment_fn=do_nothing,
     static_parameters={
         "imode": 1
     }
 )
 
+# TODO: pct only implemented in Motti
 earlyTending = PredeterminedTreatment(
     name="fc_mechanical_clearing",
-    treatment_fn=pct_fn,
+    # treatment_fn=pct_fn,
+    treatment_fn=do_nothing
 )
