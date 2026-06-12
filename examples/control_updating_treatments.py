45999fb602b164630a30209b91013d71cc4d31a6
Tapani Stylman <tapani.stylman@gofore.com>
2026-06-12 16:56:43 +0300

    add updating run mode example

---
+++ examples/control_updating_treatments.py
@@ -0,0 +1,64 @@
+from lukefi.metsi.domain.collected_data import NaturalProcessInfo
+from lukefi.metsi.domain.conditions import RelativeTimePoints
+from lukefi.metsi.domain.forestry_types import ForestCondition
+from lukefi.metsi.domain.natural_processes.updating import update_to_year
+from lukefi.metsi.domain.natural_processes.grow_acta import grow_acta_fn
+from lukefi.metsi.domain.pre_ops import filter_trees, generate_reference_trees, filter_stands, scale_area_weight
+from lukefi.metsi.sim.generators import Alternatives, Event, Sequence
+from lukefi.metsi.sim.sim_configuration import UpdatingInstructions
+from lukefi.metsi.sim.transition import Transition
+from lukefi.metsi.sim.simulation_instruction import SimulationInstruction
+from lukefi.metsi.sim.treatment import do_nothing
+from examples.declarations.export_prepro import mela_decl
+from examples.declarations.sqlite import sqlite_decl
+
+
+control_structure = {
+    "app_configuration": {
+        "state_format": "xml",  # options: fdm, vmi12, vmi13, xml, gpkg
+        "run_modes": ["preprocess", "update", "export_prepro"],
+        "preprocessing_output_file": "preprocessing_results",
+        "simulation_output_file": "simulation_results",
+        "sqlite_decl": sqlite_decl,
+    },
+    "preprocessing_operations": [
+        scale_area_weight,
+        generate_reference_trees,  # reference trees from strata, replaces existing reference trees
+        filter_stands,
+        filter_trees,
+    ],
+    "preprocessing_params": {
+        generate_reference_trees: [
+            {
+                "n_trees": 10,
+                "method": "weibull",
+                "debug": False
+            }
+        ],
+        filter_stands: [
+            {
+                "remove": (lambda stand: (stand.site_type_category is None) or (stand.site_type_category == 0))
+            }
+        ],
+        filter_trees: [
+            {
+                "predicate": lambda stand: ~stand.reference_trees.sapling & (stand.reference_trees.stems_per_ha > 0)
+            }
+        ]
+    },
+    "updating": UpdatingInstructions(
+        2026,
+        grow_acta_fn,
+        True,
+        True,
+        True,
+        True
+    ),
+    'export_prepro': {
+        'csv': {},
+        'csv_exp': {},
+        'rst': mela_decl
+    }
+}
+
+__all__ = ['control_structure']
