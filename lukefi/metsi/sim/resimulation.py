bff4ceb793b87dff9b73e7bc099fe27b9b377bed
Tapani Stylman <tapani.stylman@gofore.com>
2026-06-18 16:13:48 +0300

    wip implementation of initial state reconstruction

--- lukefi/metsi/sim/resimulation.py
+++ lukefi/metsi/sim/resimulation.py
@@ -1,9 +1,13 @@
 from copy import deepcopy
 import csv
+import numpy as np
+import numpy.typing as npt
 import sqlite3
 from typing import Any, Set
 
+from lukefi.metsi.data.enums.internal import CuttingMethod, DevelopmentClass, DrainageCategory, DrainedPeatlandForestType, FraLandUseClass, OwnerCategory, PeatlandForestType, SiteType, SoilPeatlandCategory
 from lukefi.metsi.data.model import ForestStand
+from lukefi.metsi.data.vector_model import ReferenceTrees, TreeStrata
 from lukefi.metsi.sim.simulation_payload import SimulationPayload
 from lukefi.metsi.sim.transition import TransitionFn
 from lukefi.metsi.sim.treatment import PredeterminedTreatment, TreatmentFn
@@ -57,9 +61,6 @@ def resimulate_schedules(control: dict[str, Any],
 
 
 def _determine_transition(control: dict[str, Any], in_db: sqlite3.Connection) -> TransitionFn[ForestStand]:
-    _ = control
-    _ = in_db
-    assert False, "_determine_transition not implemented"
     return control["transition"]
 
 
@@ -156,12 +157,142 @@ def _parse_params(params_str: str) -> dict[str, Any]:
 
 
 def _reconstruct_initial_state(stand_id: str, in_db: sqlite3.Connection) -> ForestStand:
-    # TODO: how to reconstruct initial state if and when original simulation db has incomplete attributes?
-    #           - always complete output for initial state?
-    #               - would lead to lots of mostly empty columns...
-    #               - unless we add new tables specifically for the initial state? <- THIS
-    #           - require original source data or preprocessed csv?
-    #               - how to deal with potential updating?
-    _ = stand_id
-    _ = in_db
-    assert False, "_reconstruct_initial_state not implemented"
+    cur = in_db.cursor()
+    cur.row_factory = sqlite3.Row
+    cur.execute(
+        """--sql
+            SELECT * FROM initial_stands
+            WHERE
+                identifier = ?;
+        """,
+        (
+            stand_id
+        )
+    )
+    stand_row = cur.fetchone()
+
+    cur.row_factory = None
+    cur.execute(
+        """--sql
+            SELECT COUNT(*) FROM initial_trees
+            WHERE
+                stand = ?;
+        """,
+        (
+            stand_id
+        )
+    )
+    tree_count = cur.fetchone()[0]
+    trees = ReferenceTrees()
+    trees.size = tree_count
+
+    trees.identifier = np.array(_fetch_initial_trees_col(stand_id, "identifier", cur), dtype=np.dtype("U30"))
+    trees.tree_number = np.array(_fetch_initial_trees_col(stand_id, "tree_number", cur), dtype=np.int32)
+    trees.species = np.array(_fetch_initial_trees_col(stand_id, "species", cur), dtype=np.int32)
+    trees.breast_height_diameter = np.array(_fetch_initial_trees_col(
+        stand_id, "breast_height_diameter", cur), dtype=np.float64)
+    trees.height = np.array(_fetch_initial_trees_col(stand_id, "height", cur), dtype=np.float64)
+    trees.measured_height = np.array(_fetch_initial_trees_col(stand_id, "measured_height", cur), dtype=np.float64)
+    trees.breast_height_age = np.array(_fetch_initial_trees_col(stand_id, "breast_height_age", cur), dtype=np.float64)
+    trees.biological_age = np.array(_fetch_initial_trees_col(stand_id, "biological_age", cur), dtype=np.float64)
+    trees.stems_per_ha = np.array(_fetch_initial_trees_col(stand_id, "stems_per_ha", cur), dtype=np.float64)
+    trees.origin = np.array(_fetch_initial_trees_col(stand_id, "origin", cur), dtype=np.int32)
+    trees.management_category = np.array(_fetch_initial_trees_col(stand_id, "management_category", cur), dtype=np.int32)
+    trees.tree_category = np.array(_fetch_initial_trees_col(stand_id, "tree_category", cur), dtype=np.dtype("U1"))
+    trees.storey = np.array(_fetch_initial_trees_col(stand_id, "storey", cur), dtype=np.int32)
+    trees.sapling = np.array(_fetch_initial_trees_col(stand_id, "sapling", cur), dtype=np.bool_)
+    trees.tree_type = np.array(_fetch_initial_trees_col(stand_id, "tree_type", cur), dtype=np.dtype("U1"))
+    trees.damage_type = np.array(_fetch_initial_trees_col(stand_id, "damage_type", cur), dtype=np.dtype("U2"))
+    trees.crown_class = np.array(_fetch_initial_trees_col(stand_id, "crown_class", cur), dtype=np.dtype("U1"))
+    trees.basal_area = np.array(_fetch_initial_trees_col(stand_id, "basal_area", cur), dtype=np.float64)
+    trees.volume = np.array(_fetch_initial_trees_col(stand_id, "volume", cur), dtype=np.float64)
+    trees.stratum = np.array(_fetch_initial_trees_col(stand_id, "stratum", cur), dtype=np.int32)
+
+    assert len(trees.identifier) == trees.size
+    assert len(trees.tree_number) == trees.size
+    assert len(trees.species) == trees.size
+    assert len(trees.breast_height_diameter) == trees.size
+    assert len(trees.height) == trees.size
+    assert len(trees.measured_height) == trees.size
+    assert len(trees.breast_height_age) == trees.size
+    assert len(trees.biological_age) == trees.size
+    assert len(trees.stems_per_ha) == trees.size
+    assert len(trees.origin) == trees.size
+    assert len(trees.management_category) == trees.size
+    assert len(trees.tree_category) == trees.size
+    assert len(trees.storey) == trees.size
+    assert len(trees.sapling) == trees.size
+    assert len(trees.tree_type) == trees.size
+    assert len(trees.damage_type) == trees.size
+    assert len(trees.crown_class) == trees.size
+    assert len(trees.basal_area) == trees.size
+    assert len(trees.volume) == trees.size
+    assert len(trees.stratum) == trees.size
+
+    retval = ForestStand(
+        reference_trees=trees,
+        tree_strata=TreeStrata(),
+        motti_state=None,
+        time=stand_row["year"],
+        start_time=stand_row["year"],
+        identifier=stand_row["identifier"],
+        stand_id=stand_row["stand_id"],
+        area=stand_row["area"],
+        area_weight=stand_row["area_weight"],
+        geo_location=_parse_geo_location(stand_row["geo_location"]),
+        degree_days=stand_row["degree_days"],
+        owner_category=OwnerCategory(stand_row["owner_category"]),
+        soil_peatland_category=SoilPeatlandCategory(stand_row["soil_peatland_category"]),
+        site_type_category=SiteType(stand_row["site_type_category"]),
+        tax_class_reduction=stand_row["tax_class_reduction"],
+        tax_class=stand_row["tax_class"],
+        drainage_category=DrainageCategory(stand_row["drainage_category"]),
+        drainage_year=stand_row["drainage_year"],
+        fertilization_year=stand_row["fertilization_year"],
+        soil_surface_preparation_year=stand_row["soil_surface_preparation_year"],
+        regeneration_area_cleaning_year=stand_row["regeneration_area_cleaning_year"],
+        development_class=DevelopmentClass(stand_row["development_class"]),
+        artificial_regeneration_year=stand_row["artificial_regeneration_year"],
+        young_stand_tending_year=stand_row["young_stand_tending_year"],
+        cutting_year=stand_row["cutting_year"],
+        forestry_centre_id=stand_row["forestry_centre_id"],
+        forest_management_category=stand_row["forest_management_category"],
+        method_of_last_cutting=CuttingMethod(stand_row["method_of_last_cutting"]),
+        municipality_id=stand_row["municipality_id"],
+        ds_main_tree_species_biological_age=stand_row["ds_main_tree_species_biological_age"],
+        area_weight_factors=stand_row["area_weight_factors"],
+        fra_category=FraLandUseClass(stand_row["fra_category"]),
+        auxiliary_stand=bool(stand_row["auxiliary_stand"]),
+        sea_effect=stand_row["sea_effect"],
+        lake_effect=stand_row["lake_effect"],
+        basal_area=stand_row["basal_area"],
+        main_tree_species_dominant_storey=stand_row["main_tree_species_dominant_storey"],
+        ds_dominant_height=stand_row["ds_dominant_height"],
+        region=stand_row["region"],
+        peatland_type=PeatlandForestType(stand_row["peatland_type"]),
+        drained_peatland_type=DrainedPeatlandForestType(stand_row["drained_peatland_type"]),
+        under_storey=bool(stand_row["under_storey"]),
+        over_storey=bool(stand_row["over_storey"])
+    )
+
+    return retval
+
+
+def _parse_geo_location(src: str) -> tuple[float | None, float | None, float | None, str | None] | None:
+    _ = src
+    return None
+
+
+def _fetch_initial_trees_col(stand: str, col: str, cur: sqlite3.Cursor) -> list[Any]:
+    cur.execute(
+        """--sql
+            SELECT ? FROM initial_trees
+            WHERE
+                stand = ?;
+        """,
+        (
+            col,
+            stand
+        )
+    )
+    return cur.fetchall()
