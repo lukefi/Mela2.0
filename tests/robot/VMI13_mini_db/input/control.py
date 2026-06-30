27e3d9250eaad7c7d386051c1d74694598593d17
Tapani Stylman <tapani.stylman@gofore.com>
2026-07-01 02:22:40 +0300

    update VMI13_mini_db robot test control

--- tests/robot/VMI13_mini_db/input/control.py
+++ tests/robot/VMI13_mini_db/input/control.py
@@ -1,59 +1,63 @@
+from lukefi.metsi.app.metsi_enum import RunMode, StateFormat
 from lukefi.metsi.data.model import ForestStand
 from lukefi.metsi.domain.conditions import TimePoints
 from lukefi.metsi.domain.events import GrowActa, GrowMetsi
 from lukefi.metsi.domain.pre_ops import filter_stands, filter_trees, generate_reference_trees, scale_area_weight
 from lukefi.metsi.sim.condition import Condition
 from lukefi.metsi.sim.generators import Alternatives
-from lukefi.metsi.sim.operations import do_nothing
-from lukefi.metsi.sim.transition import Transition
 from lukefi.metsi.sim.instructions import SimulationInstruction
+from lukefi.metsi.sim.operations import do_nothing
+from lukefi.metsi.sim.sim_control import AppConfiguration, MetsiControl, Preprocessing, Simulation
+from lukefi.metsi.sim.transition import Transition
 
 
-control_structure = {
-    "app_configuration": {
-        "state_format": "vmi13",
-        "run_modes": ["preprocess", "simulate"]
-    },
-    "preprocessing_operations": [
-        scale_area_weight,
-        generate_reference_trees,
-        filter_stands,
-        filter_trees
-    ],
-    "preprocessing_params": {
-        generate_reference_trees: [
-            {
-                "n_trees": 10,
-                "method": "weibull",
-                "debug": False,
-                "delete_strata": True
-            }
+control_structure = MetsiControl[ForestStand](
+    app_configuration=AppConfiguration(
+        state_format=StateFormat.VMI13,
+        run_modes=[RunMode.PREPROCESS, RunMode.SIMULATE]
+    ),
+    preprocessing=Preprocessing[ForestStand](
+        operations=[
+            scale_area_weight,
+            generate_reference_trees,
+            filter_stands,
+            filter_trees
         ],
-        filter_stands: [
-            {
-                "remove": (lambda stand: (stand.site_type_category is None) or (stand.site_type_category == 0))
-            }
-        ],
-        filter_trees: [
-            {
-                "predicate": (lambda stand: ~((stand.reference_trees.sapling != 0) |
-                                              (stand.reference_trees.stems_per_ha == 0)))
-            }
-        ]
-    },
-    "simulation_instructions": [
-        SimulationInstruction(
-            conditions=[TimePoints([2018, 2023, 2028])],
-            events=[
-                Alternatives([
-                    GrowActa(),
-                    GrowMetsi(),
-                ])
+        params={
+            generate_reference_trees: [
+                {
+                    "n_trees": 10,
+                    "method": "weibull",
+                    "debug": False,
+                    "delete_strata": True
+                }
+            ],
+            filter_stands: [
+                {
+                    "remove": (lambda stand: (stand.site_type_category is None) or (stand.site_type_category == 0))
+                }
+            ],
+            filter_trees: [
+                {
+                    "predicate": (lambda stand: ~((stand.reference_trees.sapling != 0) |
+                                                  (stand.reference_trees.stems_per_ha == 0)))
+                }
             ]
-        )
-    ],
-    "transition": Transition(do_nothing, 5, db_output=False),
-    "end_condition": Condition[ForestStand](lambda x: x.computational_unit.time > 2028)
-}
-
-__all__ = ['control_structure']
+        },
+    ),
+    simulation=Simulation[ForestStand](
+        instructions=[
+            SimulationInstruction(
+                conditions=[TimePoints([2018, 2023, 2028])],
+                events=[
+                    Alternatives([
+                        GrowActa(),
+                        GrowMetsi(),
+                    ])
+                ]
+            )
+        ],
+        transition=Transition(do_nothing, 5, db_output=False),
+        end_condition=Condition[ForestStand](lambda x: x.computational_unit.time > 2028)
+    )
+)
