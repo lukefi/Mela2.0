fda9d42f428f6031d46c898a75a86fb3aa1f600b
Tapani Stylman <tapani.stylman@gofore.com>
2026-07-02 15:37:48 +0300

    move resimulation control to examples

--- examples/control_resimulation.py
+++ examples/control_resimulation.py
@@ -1,9 +1,12 @@
 from lukefi.metsi.data.model import ForestStand
+from lukefi.metsi.domain.natural_processes import grow_motti_dll
 from lukefi.metsi.domain.natural_processes.grow_acta import grow_acta_fn
+from lukefi.metsi.domain.natural_processes.grow_motti_dll import grow_motti_dll_fn
 from lukefi.metsi.sim.instructions import ResimulationInstructions
 from lukefi.metsi.sim.operations import do_nothing
 from lukefi.metsi.domain.collected_data import NaturalProcessInfo
 from examples.declarations.sqlite import sqlite_decl
+from lukefi.metsi.sim.transition import Transition
 
 
 control_structure = {
@@ -14,7 +17,7 @@ control_structure = {
         "sqlite_decl": sqlite_decl,
     },
     "resimulation": ResimulationInstructions[ForestStand](
-        transition=grow_acta_fn,
+        transition=Transition(grow_motti_dll_fn, max_step=5, collected_data={NaturalProcessInfo}, name="grow_motti"),
         schedules_file="selected_schedules.csv",
         treatment_map={
             "do_nothing": do_nothing,
